92c7a7b82e571cab12c1efba51558602a4b313b9
Marc Freudenberg <marc.freudenberg@mobilityhouse.com>
2023-01-29 19:43:40 +0100

    disable api proxy origin check

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,18 +5,23 @@ import { error } from '@sveltejs/kit';
 import type { RequestInit } from 'undici';
 import { fetch } from 'undici';
 
-const MY_API_BASE_URL = 'http://localhost:8080';
+const MY_API_BASE_URL = process.env.API_PROXY_DEST_URL;
 const PROXY_PATH = '/api-proxy';
 
 // TODO: a bunch of type errors here, it seems to be fine at runtime, though
 
 const handleApiProxy: Handle = async ({ event }) => {
-	const origin = event.request.headers.get('Referer');
+	/*
+	TODO: fix this same origin check here at some point.
+	const origin = event.request.headers.get('Origin');
 
 	// reject requests that don't come from the webapp, to avoid your proxy being abused.
 	if (!origin || new URL(origin).origin !== event.url.origin) {
+		console.log('Request Origin header', origin);
+		console.log('Rejecting request event:', event);
 		throw error(403, 'Request Forbidden.');
 	}
+	*/
 
 	// strip `/api-proxy` from the request path
 	const strippedPath = event.url.pathname.substring(PROXY_PATH.length);
