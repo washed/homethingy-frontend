eca749049ef57193d14db2913723439f0615de32
Marc Freudenberg <marc.freudenberg@mobilityhouse.com>
2023-01-30 11:36:59 +0100

    add tab route reactivity

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 	import 'carbon-components-svelte/css/all.css';
 	import { Theme, Grid, Row, Column, Toggle, Tabs, Tab } from 'carbon-components-svelte';
 
@@ -14,15 +15,34 @@
 			theme = 'g10';
 		}
 	};
+
+	type RouteDef = {
+		label: string;
+		href: string;
+	};
+
+	const TAB_ROUTES: RouteDef[] = [
+		{
+			label: 'Home',
+			href: '/'
+		},
+		{
+			label: 'Kitchen',
+			href: '/kitchen'
+		}
+	];
+
+	$: selected = TAB_ROUTES.findIndex((value) => value.href == $page.route.id);
 </script>
 
 <Theme bind:theme persist persistKey="__carbon-theme" />
 
 <div class="flexed">
 	<div style="display: contents;">
-		<Tabs>
-			<Tab label="Home" href="/" on:click={() => goto('/')} />
-			<Tab label="Kitchen" href="/kitchen" on:click={() => goto('/kitchen')} />
+		<Tabs {selected}>
+			{#each TAB_ROUTES as { label, href }, i}
+				<Tab {label} {href} on:click={() => goto(href)} />
+			{/each}
 		</Tabs>
 	</div>
 	<div style="min-width: 6rem;">
