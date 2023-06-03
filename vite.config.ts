90f9bf1c0c7ec7d308711a92f3330a8230c890f4
Marc Freudenberg <marc.freudenberg@mobilityhouse.com>
2023-06-03 22:00:00 +0200

    add some icon

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,27 @@ import type { UserConfig } from 'vite';
 const config: UserConfig = {
 	plugins: [
 		sveltekit(),
-		SvelteKitPWA(),
+		SvelteKitPWA({
+			includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+			manifest: {
+				name: 'homethingy-frontend',
+				short_name: 'hf',
+				description: 'My Awesome App description',
+				theme_color: '#ffffff',
+				icons: [
+					{
+						src: 'android-chrome-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: 'android-chrome-512x512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					}
+				]
+			}
+		})
 	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
