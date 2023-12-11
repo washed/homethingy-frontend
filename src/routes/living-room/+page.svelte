095f9184cdcff3c63ede8875d3d74194e64b0553
Marc Freudenberg <marc.freudenberg@mobilityhouse.com>
2023-12-11 11:16:47 +0100

    add christmas light plug to living room

--- src/routes/living-room/+page.svelte
+++ src/routes/living-room/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import HeatingUi from '$lib/HeatingUI.svelte';
+	import PlugUi from '$lib/PlugUI.svelte';
 </script>
 
 <HeatingUi controllerName="living-room" />
+<PlugUi controllerName="christmas_light" name="Christmas Light" />
